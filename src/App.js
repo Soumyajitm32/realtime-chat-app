ae4ee68d415da233681ab1683ba5567848dd21d1
Soumyajit Mandal <soumyajitm32@gmail.com>
2023-07-06 19:52:23 +0530

    Realtime Chat App-Completed

--- src/App.js
+++ src/App.js
@@ -3,40 +3,48 @@ import "./App.css";
 import { Auth } from "./Components/Auth";
 import Cookies from "universal-cookie";
 import { Chat } from "./Components/Chat";
+import { AppWrapper } from "./Components/AppWrapper";
+
 
 
 const cookies = new Cookies();
 
-function App() {
+function ChatApp() {
   const [isAuth, setIsAuth] = useState(cookies.get("auth-token"));
-  const [room, setRoom] = useState(null);
-  const roomInputRef=useRef(null)
+  const [isInChat, setIsInChat] = useState(null);
+  const [room, setRoom] = useState("");
+
   if (!isAuth) {
     return (
-      <div className="App">
-        <div>
-          <Auth setIsAuth={setIsAuth} />
-        </div>
-      </div>
+      <AppWrapper
+        isAuth={isAuth}
+        setIsAuth={setIsAuth}
+        setIsInChat={setIsInChat}
+      >
+        <Auth setIsAuth={setIsAuth} />
+      </AppWrapper>
     );
-
-    
   }
+
   return (
-    <div>
-      {room ? (
-        <div>
-          <Chat room={room}/>
+    <AppWrapper isAuth={isAuth} setIsAuth={setIsAuth} setIsInChat={setIsInChat}>
+      {!isInChat ? (
+        <div className="room">
+          <label> Type room name: </label>
+          <input onChange={(e) => setRoom(e.target.value)} />
+          <button
+            onClick={() => {
+              setIsInChat(true);
+            }}
+          >
+            Enter Chat
+          </button>
         </div>
       ) : (
-        <div className="room">
-          <label>Enter Chat Room</label>
-          <input ref={roomInputRef} />
-          <button onClick={()=>setRoom(roomInputRef.current.value)}>Enter Chat</button>
-        </div>
+        <Chat room={room} />
       )}
-    </div>
+    </AppWrapper>
   );
 }
 
-export default App;
+export default ChatApp;
